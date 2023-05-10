6458c613f90045aaf0a8c949f7f8f9b3485b1a50
prashanthPadmasahli <prashanthkumarshettigar@gmail.com>
2023-05-10 20:54:50 +0530

    Initial Commit

---
+++ TestApp-1/TestApp_1App.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp_1App.swift
+//  TestApp-1
+//
+//  Created by mac on 10/05/23.
+//
+
+import SwiftUI
+
+@main
+struct TestApp_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
